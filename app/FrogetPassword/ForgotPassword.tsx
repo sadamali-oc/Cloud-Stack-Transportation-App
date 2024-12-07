a65eaf9255b42d31b12046352dcb4acea40154f0
Chamalka Obadage <110493084+sadamali-oc@users.noreply.github.com>
2024-12-07 10:35:55 +0530

    forgot pwd

---
+++ app/FrogetPassword/ForgotPassword.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import { router } from "expo-router";
+
+const { width } = Dimensions.get("window");
+
+export default function ForgotPassword() {
+  const [username, setUsername] = useState("");
+
+
+
+  const onConfirmPressed = () => {
+    console.log("User Confirmation Successfully");
+  };
+
+  const onSendPressed = () => {
+    console.log("onResendPress");
+  };
+
+
+  
+
+  return (
+    <ScrollView
+      style={styles.scrollView}
+      contentContainerStyle={styles.scrollContent}
+    >
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Reset your Password</Text>
+
+        <CustomInput
+          placeholder="User name"
+          value={username}
+          setValue={setUsername}
+        />
+
+        <CustomButton text="Send" onPress={onSendPressed} />
+      
+      
+      
+
+
+        <CustomButton
+          text="Back to Signomng in"
+          onPress={() => {
+            router.push("/login");
+          }}
+          type="TERTIARY"
+        />
+      </SafeAreaView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#051C60",
+    margin: 10,
+    // marginVertical: 10,
+    // marginHorizontal: 20,
+    
+  },
+
+  text: {
+    color: "gray",
+    marginVertical: 10,
+    marginHorizontal: 20, // Horizontal margin (left and right)
+  },
+
+  link: {
+    color: "#FDB075",
+  },
+});
